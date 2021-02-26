a14f2dd4476b9a3a542ccdd7f8400a99cca1fd2a
unname666 <78314985+unname666@users.noreply.github.com>
2021-02-25 17:37:23 -0700

    trying to fix js code

--- script.js
+++ script.js
@@ -5,9 +5,6 @@
 // WHEN prompted for password criteria
 // THEN I select which criteria to include in the password
 
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-
 // WHEN prompted for character types to include in the password
 // THEN I choose lowercase, uppercase, numeric, and/or special characters
 
@@ -44,6 +41,7 @@ var lowcharacters = [
   "u",
   "v",
   "w",
+  "x",
   "y",
   "z",
 ];
@@ -77,13 +75,14 @@ var upperCharacters = [
 ];
 var numberCharacters = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*"];
-var randompassword = [];
 
-function generatePassword(arr, number) {
-  var newArray = arr.flat();
-  var newPassword = [];
+var password = [];
+
+function generatePassword() {
+//   var newArray = arr();
+  var Password = [];
     for (var i = 0; i < array.length; i++) {
-        var randomIndex = math.floor(math.random() * new Array.length - 1) + 1;
+        var password = math.floor(math.random() * newArray.length - 1) + 1;
         passwword.push(newArry[randomindex]);
     }
     return password.join("");
@@ -93,31 +92,31 @@ function generatePassword(arr, number) {
 function writePassword() {
 
 // need to add prompts and confirms
-var aNumber = number(window.prompt("how long to do want the password?"))
+var aNumber = window.prompt("how long to do want the password?")
     if(aNumber <8 || aNumber > 128){
         alert("please enter number 8 through 128");
         return;
     }
-    else if (NaN(numberofcharacters)){
+    else if (NaN){
         alert("please enter valid numbers")
         return;
     }
-var upperCharactersQuestion = windows.confirm("have upper case characters?")
-var lowerCharactersQuestion = windows.confirm("have low case characters?")
-var specialCharactersQuestion = windows.confirm("have special characters?")
-var numberCharactersQuestion = windows.confirm("have numbers?")
+var upperCharactersQuestion = confirm("have upper case characters?")
+var lowerCharactersQuestion = confirm("have low case characters?")
+var specialCharactersQuestion = confirm("have special characters?")
+var numberCharactersQuestion = confirm("have numbers?")
 
-if (upperCharactersQuestion){randompassword.push(upperCharacter);
-}
-if (lowerCharactersQuestion){randompassword.push(lowerCharacter);
-}
-if (specialCharactersQuestion){randompassword.push(specialCharacter);
-}
-if (numberCharactersQuestion){randompassword.push(numberCharacter);
-}
-else if(!upperCharacters && !lowerCharacters && !specialCharacters && !numberCharacters){
-    alert("please select at one character type")
-}
+// if (upperCharactersQuestion){password.push(upperCharacter);
+// }
+// if (lowerCharactersQuestion){password.push(lowerCharacter);
+// }
+// if (specialCharactersQuestion){password.push(specialCharacter);
+// }
+// if (numberCharactersQuestion){password.push(numberCharacter);
+// }
+// else if(!upperCharacters && !lowerCharacters && !specialCharacters && !numberCharacters){
+//     alert("please select at one character type")
+// }
     var password = generatePassword();
 
     var passwordText = document.querySelector("#password");
